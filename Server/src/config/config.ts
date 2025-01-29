62e440d545c47677bee3d813643c1ca1840eb2de
Yuval Dayan <ydayan232@gmail.com>
2025-01-29 10:00:45 +0200

    connected to gmail API

--- Server/src/config/config.ts
+++ Server/src/config/config.ts
@@ -1,7 +1,33 @@
+import dotenv from "dotenv";
+import path from "path";
 const envPath =
   "C:/Users/Yuval-PC/Desktop/myCode/Projects/GmailEmulationApp/Server/.env";
 
+const envPath2 =
+  "C:/Users/Yuval-PC/Desktop/myCode/Projects/GmailEmulationApp/.env";
 
+const setEnvPath = () => {
+  dotenv.config({
+    path: path.resolve(__dirname, envPath2),
+  });
+};
+
+const getSecrets = () => {
+  setEnvPath();
+  try {
+    const clientId = process.env.GOOGLE_CLIENT_ID;
+    const ClientSecret = process.env.GOOGLE_CLIENT_SECRET;
+    if (clientId && ClientSecret) return { clientId, ClientSecret };
+    else throw new Error("no keys found");
+  } catch (error) {
+    console.log(error);
+  }
+};
+setEnvPath();
+const secrets = getSecrets();
+
+export const googleClientId = secrets?.ClientSecret
+export const googleClientSecret = secrets?.ClientSecret
 export const googleOauthRedirectUrl =
   "http://localhost:3001/users/api/sessions/oauth/google";
 
--- client/src/config/config.tsx
+++ client/src/config/config.tsx
@@ -1,8 +1,35 @@
-const serverUrl : string= 'http://localhost:3001'
+import dotenv from "dotenv";
+import path from "path";
+const serverUrl: string = "http://localhost:3001";
 
+const envPath2 =
+  "C:/Users/Yuval-PC/Desktop/myCode/Projects/GmailEmulationApp/.env";
 
-export const googleOauthRedirectUrl = 'http://localhost:3001/users/api/sessions/oauth/google'
-export const EmailAppRedirectUrl = 'http://localhost:5137/feed'
+const setEnvPath = () => {
+  dotenv.config({
+    path: path.resolve(__dirname, envPath2),
+  });
+};
 
+const getSecrets = () => {
+  setEnvPath();
+  try {
+    const clientId = process.env.GOOGLE_CLIENT_ID;
+    const ClientSecret = process.env.GOOGLE_CLIENT_SECRET;
+    if (clientId && ClientSecret) return { clientId, ClientSecret };
+    else throw new Error("no keys found");
+  } catch (error) {
+    console.log(error);
+  }
+};
+setEnvPath();
+const secrets = getSecrets();
 
-export default serverUrl 
+export const googleClientId = secrets?.ClientSecret;
+export const googleClientSecret = secrets?.ClientSecret;
+
+export const googleOauthRedirectUrl =
+  "http://localhost:3001/users/api/sessions/oauth/google";
+export const EmailAppRedirectUrl = "http://localhost:5137/feed";
+
+export default serverUrl;
