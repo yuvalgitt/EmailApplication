e7945ffc5e8d70ecb1c4c3702c7c56fe78c3563d
Yuval Dayan <ydayan232@gmail.com>
2025-01-29 09:51:42 +0200

    Connected to gmail API and received first emails via API

--- Server/src/config/config.ts
+++ Server/src/config/config.ts
@@ -1,9 +1,7 @@
 const envPath =
   "C:/Users/Yuval-PC/Desktop/myCode/Projects/GmailEmulationApp/Server/.env";
 
-export const googleClientId =
-  "273896259125-1t0c0h7aliodgnq13p1dgtcnves82k45.apps.googleusercontent.com";
-export const googleClientSecret = "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";
+
 export const googleOauthRedirectUrl =
   "http://localhost:3001/users/api/sessions/oauth/google";
 
--- client/src/config/config.tsx
+++ client/src/config/config.tsx
@@ -1,8 +1,6 @@
 const serverUrl : string= 'http://localhost:3001'
 
-export const googleClientId =
-  "273896259125-1t0c0h7aliodgnq13p1dgtcnves82k45.apps.googleusercontent.com";
-export const googleClientSecret = "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";
+
 export const googleOauthRedirectUrl = 'http://localhost:3001/users/api/sessions/oauth/google'
 export const EmailAppRedirectUrl = 'http://localhost:5137/feed'
 
